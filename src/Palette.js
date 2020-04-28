e2399fb518092ac47f1d993e99b509e9bf84977b
Hiten Sidapara <hitensidapara195@gmail.com>
2020-04-28 11:25:17 +0530

    [ADD] :  create navbar in Application

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import ColorBox from './ColorBox';
-import 'rc-slider/assets/index.css'
+import Navbar from './Navbar'
 import './Palette.css';
-import Slider from 'rc-slider';
 
 export default class Palette extends Component {
     constructor(props) {
@@ -21,16 +20,7 @@ export default class Palette extends Component {
         })
         return (
             <div className="Palette">
-                <div className="slider">
-                    <Slider
-                        defaultValue={level}
-                        min={100}
-                        max={900}
-                        step={100}
-                        onAfterChange={this.changeLevel}
-                    />
-                </div>
-                {/* NavBar Comes Here */}
+                <Navbar level={level} changeLevel={this.changeLevel} />
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>

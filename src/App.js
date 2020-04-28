f43ce89a6e9e304ecece09869e9c781c34330e31
Hiten Sidapara <hitensidapara195@gmail.com>
2020-04-28 10:48:55 +0530

    [ADD] : Generate Colors and Create Slider To Choose Different Color.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import Palette from './Palette';
-import seedColors from './seedColors';
+import React, { Component } from "react";
+import Palette from "./Palette";
+import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
-function App() {
-  return (
-    <div>
-      <Palette {...seedColors[7]} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Palette palette={generatePalette(seedColors[4])} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Palette.js
+++ src/Palette.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
 import ColorBox from './ColorBox';
+import 'rc-slider/assets/index.css'
 import './Palette.css';
+import Slider from 'rc-slider';
 
 export default class Palette extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { level: 500 }
+        this.changeLevel = this.changeLevel.bind(this)
+    }
+    changeLevel(level) {
+        this.setState({ level })
+    }
     render() {
-        const colorBoxes = this.props.colors.map(color => {
-            return <ColorBox background={color.color} name={color.name} />
+        const { colors } = this.props.palette;
+        const { level } = this.state;
+        const colorBoxes = colors[level].map(color => {
+            return <ColorBox background={color.hex} name={color.name} />
         })
         return (
             <div className="Palette">
+                <div className="slider">
+                    <Slider
+                        defaultValue={level}
+                        min={100}
+                        max={900}
+                        step={100}
+                        onAfterChange={this.changeLevel}
+                    />
+                </div>
                 {/* NavBar Comes Here */}
                 <div className="Palette-colors">
                     {colorBoxes}

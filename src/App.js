aaf6f5964c322c479f81d9f8439a11faf521c7b6
Hiten Sidapara <hitensidapara195@gmail.com>
2020-04-29 10:12:23 +0530

    [ADD] : Palette More Button Get Functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Router } from 'react-router-dom';
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
@@ -18,6 +18,8 @@ class App extends Component {
         <Route exact path="/palette/:id" render={routeProps => (
           <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
         )} />
+        <Route exact path="/palette/:paletteId/:colorId"
+          render={() => <h1>Single Color Page</h1>} />
       </Switch>
       // <div>
       //   <Palette palette={generatePalette(seedColors[4])} />
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Link } from 'react-router-dom';
 import './ColorBox.css';
 
 export default class ColorBox extends Component {
@@ -32,7 +33,9 @@ export default class ColorBox extends Component {
                         </div>
                         <button className="copy-button">Copy</button>
                     </div>
-                    <span className="see-more">MORE</span>
+                    <Link to="/" onClick={(e) => e.stopPropagation()}>
+                        <span className="see-more">MORE</span>
+                    </Link>
                 </div>
             </CopyToClipboard>
         )

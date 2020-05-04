d3cf121c65c2caf2ef33c5e10c4d4bfe16e2ebaa
Hiten Sidapara <hitensidapara195@gmail.com>
2020-05-04 11:30:33 +0530

    [FIX] : Hambourgun Icone In CreatePalette Page.

--- src/styles/NewPaletteFormStyles.js
+++ src/styles/NewPaletteFormStyles.js
@@ -1,4 +1,4 @@
-import {DRAWER_WIDTH} from '../Constant';
+import { DRAWER_WIDTH } from '../Constant';
 
 const drawerWidth = DRAWER_WIDTH;
 
@@ -38,6 +38,7 @@ const styles = theme => ({
     },
     drawerHeader: {
         display: "flex",
+        width: "100%",
         alignItems: "center",
         padding: "0 8px",
         ...theme.mixins.toolbar,
@@ -46,7 +47,7 @@ const styles = theme => ({
     content: {
         flexGrow: 1,
         height: "calc(100vh - 64px)",
-        padding: theme.spacing.unit * 3,
+        padding: 0,
         transition: theme.transitions.create("margin", {
             easing: theme.transitions.easing.sharp,
             duration: theme.transitions.duration.leavingScreen
